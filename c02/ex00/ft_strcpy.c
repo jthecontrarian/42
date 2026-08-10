dc495bf4460c015c8f2c1ca2aa27c274b0231ea2
Jenn Feng Lau <jelau@u81z06s01.42kl.edu.my>
2026-08-10 12:24:49 +0800

    added all files

--- c02/ex00/ft_strcpy.c
+++ c02/ex00/ft_strcpy.c
@@ -1,30 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strncpy.c                                       :+:      :+:    :+:   */
+/*   ft_strcpy.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jelau <jelau@student.42kl.edu.my>          +#+  +:+       +#+        */
+/*   By: jelau <jelau@student.42.edu.my>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/08/10 10:04:45 by jelau             #+#    #+#             */
-/*   Updated: 2026/08/10 11:45:22 by jelau            ###   ########.fr       */
+/*   Created: 2026/08/07 12:21:06 by jelau             #+#    #+#             */
+/*   Updated: 2026/08/07 18:57:18 by jelau            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char	*ft_strncpy(char *dest, char *src, unsigned int n)
+char	*ft_strcpy(char *dest, char *src)
 {
 	int	i;
 
 	i = 0;
-	while (src[i] != '\0' && i < n)
+	while (src[i] != '\0')
 	{
 		dest[i] = src[i];
 		i++;
 	}
-	while (i < n)
-	{
-		dest[i] = '\0';
-		i++;
-	}
+	dest[i] = '\0';
 	return (dest);
 }
 
@@ -34,17 +30,17 @@ char	*ft_strncpy(char *dest, char *src, unsigned int n)
 
 int main()
 {   
-    // example of strncpy() from <string.h>
+    // example of strcpy() from <string.h>
     char a[] = "aaaaa";
     char b[] = "bbb";
-    printf("%s\n", strncpy(a, b, 2)); //dest,src,number
+    printf("%s\n", strcpy(a, b, 2)); //dest,src,number
     printf("dest=%s\nsrc=%s\n", a, b );
     printf("\n");
     
-    // testing ft_strncpy()
+    // testing ft_strcpy()
     char c[] = "aaaaa";
     char d[] = "bbb";
-    printf("%s\n", ft_strncpy(c, d, 2));
+    printf("%s\n", ft_strcpy(c, d, 2));
     printf("dest=%s\nsrc=%s\n", c, d ); 
     
     return (0);
